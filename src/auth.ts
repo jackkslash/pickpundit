aa6440a0901425963c1269c2be62ce7ba1268127
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-05-22 23:23:43 +0100

    basic google auth setup with drizzle adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,15 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import NextAuth from "next-auth"
 import db from "./db"
+import google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [google],
     adapter: DrizzleAdapter(db),
-    providers: [],
+    callbacks: {
+        async session({ session, user }) {
+            session.user.id = user.id
+            return session
+        },
+    },
 })
--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres"
 import * as schema from "./schema"
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL!,
+    connectionString: process.env.AUTH_DRIZZLE_URL!,
 })
 
 const db = drizzle(pool, { schema }) as NodePgDatabase<typeof schema>
