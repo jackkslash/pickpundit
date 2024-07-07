d20aef12bfc7ce6741a9efd850c011a35eabb87a
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-07-07 13:41:23 +0100

    added user predictions to profile

--- src/app/(routes)/me/page.tsx
+++ src/app/(routes)/me/page.tsx
@@ -1,5 +1,8 @@
 import { auth, signOut } from "@/auth"
+import db from "@/db";
+import { predictions, standingPredictions } from "@/db/schema";
 import { redirect } from "next/navigation";
+import { eq } from "drizzle-orm/expressions";
 
 export default async function Home() {
     const session = await auth();
@@ -7,6 +10,12 @@ export default async function Home() {
     if (session == null) {
         redirect("/")
     }
+
+    const userPredictions = await db.select().from(predictions)
+        .where(eq(predictions.userId, session?.user?.id as string))
+
+    const userStandingPrediction = await db.select().from(standingPredictions)
+        .where(eq(standingPredictions.userId, session?.user?.id as string))
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -20,6 +29,10 @@ export default async function Home() {
                 >
                     <button type="submit">Sign Out</button>
                 </form>
+                <div>Predictions</div>
+                <pre>{JSON.stringify(userPredictions, null, 2)}</pre>
+                <div>Standing Predictions</div>
+                <pre>{JSON.stringify(userStandingPrediction, null, 2)}</pre>
             </div>
         </main>
     );
